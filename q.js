3275b6d217ca799fbe915ca19e318b85fdcb1766
Ronny Orbach <r@ronnyo.com>
2012-04-15 14:57:47 +0300

    way simpler version

--- q.js
+++ q.js
@@ -1,24 +1,15 @@
 (function(){
-
-window.q = q || [];
-var q = q;
-var oldPush = q.push;
-
-var emptyQ = function(){
-  for (var i = 0, l = q.length; i < l; i++){
-    var el = q[i];
-    var func = window[ el[0] ];
-    if (typeof func === 'function') {
-        func.apply(func, el.slice(1));
-    }
-  }
-};
-var run = function(){};
-
-q.push = function(/* string */ method /* , optional args */){
-
-};
-
-emptyQ();
-
-})()
+	// just run any passed function. no apply trickery involved, this is intended for annonymus wrappers rather than your actual code
+	function _run(func){
+		if(typeof func==='function') {
+			func();
+		}
+	}
+	// run any existing functions in the q array
+	var i = q.length;
+	for(; i > 0; i--) {
+		_run(q[i]);
+	}
+	// replace its native push method with our run utility
+	q.push = _run;
+})();
