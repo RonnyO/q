fb7c656f9f63af69f5f795356c5c0244baad270a
Ronny Orbach <r@ronnyo.com>
2012-04-15 15:25:18 +0300

    run functions in proper order; better demo example

--- q.js
+++ q.js
@@ -1,13 +1,13 @@
 (function(){
-	// just run any passed function. no apply trickery involved, this is intended for annonymus wrappers rather than your actual code
+	// just run any passed function. no scope trickery involved, this is intended for annonymus wrappers rather than your actual code
 	function _run(func){
 		if(typeof func==='function') {
 			func();
 		}
 	}
 	// run any existing functions in the q array
-	var i = q.length;
-	for(; i > 0; i--) {
+	var i = 0, l = q.length;
+	for(; i < l; i++) {
 		_run(q[i]);
 	}
 	// replace its native push method with our run utility
